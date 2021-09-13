03e3ee5359f1858e4b4de07b4f8a376bb2ca80ed
prashishb <prashb.web@gmail.com>
2021-09-12 20:37:02 -0400

    completed task 3

--- index.js
+++ index.js
@@ -91,9 +91,19 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(cb, num){
+  let homeScore = 0;
+  let awayScore = 0;
+
+  for (let i = 0; i < num; i++) {
+    homeScore += cb();
+    awayScore += cb();
+  }
+  return {'Home': homeScore, 'Away': awayScore};
 }
+
+console.log(finalScore(inning, 9));
+
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
